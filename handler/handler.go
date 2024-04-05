3b72f476d3f96875909792a0c12094bc985e13e1
zVitorSantos <vitorsantos4736@gmail.com>
2024-04-04 21:55:14 -0300

    feat: add createOpening handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/zVitorSantos/gopportunities.git/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
--- router/routes.go
+++ router/routes.go
@@ -6,10 +6,12 @@ import (
 )
 
 func initializeRoutes(r *gin.Engine) {
+	// Initialize Handler
+	handler.InitializeHandler()
 	v1 := r.Group("/api/v1")
 	{
-		v1.GET("/opening", handler.CreateOpeningHandler)
-		v1.POST("/opening", handler.ShowOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningHandler)
